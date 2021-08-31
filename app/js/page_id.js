ac51fabc4f64e9d40d8e80209b2a425526a485bc
Pedro Miraldo <pedro.miraldo@tecnico.ulisboa.pt>
2021-09-01 00:07:23 +0100

    top bar mobile OK + settings for the index.html working

---
+++ app/js/page_id.js
@@ -0,0 +1,39 @@
+const id_page_home = document.querySelector("#go_home");
+const id_page_pub = document.querySelector("#go_publications");
+const id_page_students = document.querySelector("#go_students");
+const id_page_home_down = document.querySelector("#go_home_down");
+const id_page_pub_down = document.querySelector("#go_publications_down");
+const id_page_students_down = document.querySelector("#go_students_down");
+
+window.onload = function(){
+
+    console.log(window.location.hostname)
+    console.log(window.location.pathname)
+
+    if (window.location.pathname == "/index.html" || window.location.pathname == "/" ) {
+        id_page_home.classList.add("selected");
+        id_page_pub.classList.remove("selected");
+        id_page_students.classList.remove("selected");
+        id_page_home_down.classList.add("selected");
+        id_page_pub_down.classList.remove("selected");
+        id_page_students_down.classList.remove("selected");
+    }
+
+    if (window.location.pathname == "/publications.html") {
+        id_page_home.classList.remove("selected");
+        id_page_pub.classList.add("selected");
+        id_page_students.classList.remove("selected");
+        id_page_home_down.classList.remove("selected");
+        id_page_pub_down.classList.add("selected");
+        id_page_students_down.classList.remove("selected");
+    }
+
+    if (window.location.pathname == "/students.html") {
+        id_page_home.classList.remove("selected");
+        id_page_pub.classList.remove("selected");
+        id_page_students.classList.add("selected");
+        id_page_home_down.classList.remove("selected");
+        id_page_pub_down.classList.remove("selected");
+        id_page_students_down.classList.add("selected");
+    }
+}
--- app/js/script.js
+++ app/js/script.js
@@ -2,17 +2,21 @@ const hamburger_menu = document.querySelector("#hamburguer_button");
 const menu_down = document.querySelector("#hamburguer_menu_link");
 const nav_header = document.querySelector("#nav_id");
 
-hamburger_menu.addEventListener("click", function(){
+
+hamburger_menu.addEventListener("click", function () {
     console.log("clock hamburguer");
 
-    if ( hamburger_menu.classList.contains("open") ){
+    console.log(window.location.hostname)
+    console.log(window.location.pathname)
+
+    if (hamburger_menu.classList.contains("open")) {
         hamburger_menu.classList.remove("open");
-        menu_down.classList.add("close");
+        menu_down.classList.add("closed");
         nav_header.classList.remove("remove_radius");
     }
-    else{
+    else {
         hamburger_menu.classList.add("open");
-        menu_down.classList.remove("close");
+        menu_down.classList.remove("closed");
         nav_header.classList.add("remove_radius");
     }
 });
