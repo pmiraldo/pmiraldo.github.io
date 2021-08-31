f40323ad2529da85fbf77e21850e19c1c996bece
Pedro Miraldo <pedro.miraldo@tecnico.ulisboa.pt>
2021-08-31 10:10:08 +0100

    new header included. Missing animations and the mergind of the older configs for the rest.

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+const hamburger_menu = document.querySelector("#hamburguer_button");
+const menu_down = document.querySelector("#hamburguer_menu_link");
+const nav_header = document.querySelector("#nav_id");
+
+hamburger_menu.addEventListener("click", function(){
+    console.log("clock hamburguer");
+
+    if ( hamburger_menu.classList.contains("open") ){
+        hamburger_menu.classList.remove("open");
+        menu_down.classList.add("close");
+        nav_header.classList.remove("remove_radius");
+    }
+    else{
+        hamburger_menu.classList.add("open");
+        menu_down.classList.remove("close");
+        nav_header.classList.add("remove_radius");
+    }
+});
+
