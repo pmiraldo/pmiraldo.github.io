58e561bbb216a7e91ba08077d5a45175c7ef8fe6
Pedro Miraldo <pedro.miraldo@tecnico.ulisboa.pt>
2021-09-04 23:26:44 +0100

    [update style] new style v1 ready.

--- app/js/script.js
+++ app/js/script.js
@@ -6,19 +6,17 @@ const header = document.querySelector("#header_id");
 
 hamburger_menu.addEventListener("click", function (){
 
-    console.log("blabla");
-
     if (hamburger_menu.classList.contains("open")) {
         hamburger_menu.classList.remove("open");
-        menu_down.classList.add("closed");
-        nav_header.classList.remove("remove_radius");
+        menu_down.classList.add("header_menu_links_closed");
+        header.classList.remove("remove_radius");
         header.classList.remove("header_show_down_menu");
         body.classList.remove("stop_scroll");
     }
     else {
         hamburger_menu.classList.add("open");
-        menu_down.classList.remove("closed");
-        nav_header.classList.add("remove_radius");
+        menu_down.classList.remove("header_menu_links_closed");
+        header.classList.add("remove_radius");
         header.classList.add("header_show_down_menu");
         body.classList.add("stop_scroll");
     }
